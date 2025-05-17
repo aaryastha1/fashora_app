c2f6cece6582639074c28fd89a909c9b78a151bf
Your Name <your.email@example.com>
2025-05-17 21:38:29 +0545

    home

--- lib/app.dart
+++ lib/app.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Fashora App',
-      home: FashoraHomeScreen(),
+      home: SplashScreen(),
     
       debugShowCheckedModeBanner: false,
     );
