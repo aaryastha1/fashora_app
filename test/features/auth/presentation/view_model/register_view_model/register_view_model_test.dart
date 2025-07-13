0b5e84edd548d7e55b716c9ba9a00d1b83a15dfc
aaryastha1 <your.email@example.com>
2025-07-13 14:00:43 +0545

    test update

---
+++ test/features/auth/presentation/view_model/register_view_model/register_view_model_test.dart
@@ -0,0 +1,71 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:fashora_app/features/auth/domain/use_case/user_register_usecase.dart';
+import 'package:fashora_app/features/auth/presentation/view_model/register_view_model/register_event.dart';
+import 'package:fashora_app/features/auth/presentation/view_model/register_view_model/register_state.dart';
+import 'package:fashora_app/features/auth/presentation/view_model/register_view_model/register_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockRegisterUseCase extends Mock implements UserRegisterUsecase {}
+
+class FakeRegisterUserParams extends Fake implements RegisterUserParams {}
+
+void main() {
+  late MockRegisterUseCase mockUseCase;
+
+  setUpAll(() {
+    registerFallbackValue(FakeRegisterUserParams());
+  });
+
+  setUp(() {
+    mockUseCase = MockRegisterUseCase();
+  });
+
+  testWidgets('RegisterViewModel emits loading and success and shows snackbar', (tester) async {
+    when(() => mockUseCase.call(any()))
+        .thenAnswer((_) async => const Right(true));
+
+    final viewModel = RegisterViewModel(registerUsecase: mockUseCase);
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: BlocProvider.value(
+            value: viewModel,
+            child: Builder(
+              builder: (context) {
+                return Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      context.read<RegisterViewModel>().add(
+                            RegisterUserEvent(
+                              name: 'John',
+                              email: 'john@example.com',
+                              phoneNumber: '9800000000',
+                              password: 'secret123',
+                              context: context,
+                            ),
+                          );
+                    },
+                    child: const Text('Register'),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+
+    await tester.tap(find.text('Register'));
+    await tester.pump(); // let bloc emit loading
+    await tester.pump(const Duration(seconds: 1)); // allow snackbar to appear
+
+    // ✅ Assertion
+    expect(find.byType(SnackBar), findsOneWidget);
+    expect(find.text(' Registered successful!'), findsOneWidget);
+  });
+}
