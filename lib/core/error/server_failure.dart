7f7ebbf868b77dcb533d846d5277b181737ca25b
aaryastha1 <your.email@example.com>
2025-07-31 18:25:52 +0545

    update

--- lib/core/error/server_failure.dart
+++ lib/core/error/server_failure.dart
@@ -1,11 +1,4 @@
 
-// // core/error/server_failure.dart
-
-// import 'failure.dart';
-
-// class ServerFailure extends Failure {
-//   const ServerFailure(String s, {required super.message});
-// }
 
 
 import 'package:fashora_app/core/error/failure.dart';
